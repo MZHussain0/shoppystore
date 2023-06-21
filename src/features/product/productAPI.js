5cdb3e9a33554cf0ed4c23508dae71dcf32bc996
zak <mzhussain092@gmnail.com>
2023-06-21 17:23:56 +0530

    implement filter and sort logic

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,9 +1,25 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  // return new Promise((resolve) => {
-  //   const response = await fetch('http://localhost:8080')
-  //   const data = await response.json()
-  //   resolve({data})
-  //   }
-  // );
+export function fetchAllProducts() {
+  return new Promise(async (resolve) => {
+    // TODO: Do not hard code api link
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchAllProductsByFilters(filter) {
+  // filter = {"category": "electronics"}
+  // TODO: support multiple filters
+  let queryString = "";
+  for (let key in filter) {
+    queryString += `${key}=${filter[key]}&`;
+  }
+  return new Promise(async (resolve) => {
+    const response = await fetch(
+      `http://localhost:8080/products?${queryString}`
+    );
+    const data = await response.json();
+    resolve({ data });
+  });
 }
